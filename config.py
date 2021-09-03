2d339ae985b12bf70cc24a3c36cb9ec9ce8c3d7d
musicyt <90019520+musicyt@users.noreply.github.com>
2021-09-03 09:26:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = "7673872"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "73c73f3a460335873090e0ab9ed691dd" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "1969809422:AAGzgX2xAg4jjWo91SJ1pqY0XA1KojnvxRc" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
